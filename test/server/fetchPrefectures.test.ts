5dec757d898946fbb0b5b5fdad90e65355b55762
Sijia Wang <sijiaoh@outlook.com>
2022-04-20 04:10:48 +0900

    Add test/samplePrefectures instead of sample.json.

--- test/server/fetchPrefectures.test.ts
+++ test/server/fetchPrefectures.test.ts
@@ -1,13 +1,8 @@
-import fs from "fs/promises";
 import { fetchPrefectures } from "../../src/server/fetchPrefectures";
+import { samplePrefectures } from "../samplePrefectures";
 
 test("Fetch prefectures data from resas api.", async () => {
   const prefectures = await fetchPrefectures();
 
-  const sampleJson = await fs
-    .readFile("./test/server/fetchPrefectures/sample.json")
-    .then((buffer) => buffer.toString());
-  const sample = JSON.parse(sampleJson);
-
-  expect(prefectures).toEqual(sample);
+  expect(prefectures).toEqual(samplePrefectures);
 });
